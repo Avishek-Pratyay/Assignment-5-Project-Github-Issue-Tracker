ee11895da82dc7a255f4348dff3ef582778d5047
Avishek-Pratyay <avishekp.aiub@gmail.com>
2026-03-08 00:26:27 +0600

    dashboard done

--- js/dashboard.js
+++ js/dashboard.js
@@ -30,44 +30,103 @@ container.innerHTML = ""
 let filtered = issues
 
 if(currentTab === "open"){
-filtered = issues.filter(i=>i.status==="open")
+filtered = issues.filter(i => i.status === "open")
 }
 
 if(currentTab === "closed"){
-filtered = issues.filter(i=>i.status==="closed")
+filtered = issues.filter(i => i.status === "closed")
 }
 
 document.getElementById("issueCount").innerText =
 filtered.length + " Issues"
 
-filtered.forEach(issue=>{
+filtered.forEach(issue => {
 
 const border =
 issue.status === "open"
 ? "border-open"
 : "border-closed"
 
-container.innerHTML +=
+const statusIcon =
+issue.status === "open"
+? "assets/Open-Status.png"
+: "assets/Closed-Status.png"
 
-`
+const priorityColor =
+issue.priority === "HIGH"
+? "bg-red-100 text-red-600 border border-red-300 rounded-full px-2 py-1"
+: issue.priority === "MEDIUM"
+? "bg-yellow-100 text-yellow-700 border border-yellow-300 rounded-full px-2 py-1"
+: "bg-gray-100 text-gray-600 border border-gray-300 rounded-full px-2 py-1"
+
+const label = issue.label ? issue.label : ""
+
+container.innerHTML += `
 
 <div class="issue-card ${border}" onclick="openModal(${issue.id})">
 
-<h3 class="font-semibold mb-2">${issue.title}</h3>
+<!-- top row -->
 
-<p class="text-sm text-gray-600 mb-2">
-${issue.description.substring(0,60)}...
-</p>
+<div class="flex justify-between items-center mb-2">
 
-<p class="text-xs">Author: ${issue.author}</p>
-<p class="text-xs">Priority: ${issue.priority}</p>
+<img src="${statusIcon}" class="w-4">
+
+<span class="text-xs px-2 py-1 rounded ${priorityColor}">
+${issue.priority}
+</span>
 
 </div>
+
+<!-- title -->
+
+<h3 class="font-semibold text-sm mb-1">
+${issue.title}
+</h3>
+
+<!-- description -->
+
+<p class="text-xs text-gray-500 mb-3">
+${issue.description.substring(0,80)}...
+</p>
+
+<!--label-->
+
+<div class="flex gap-3 mb-3 text-xs">
+
+<span class="px-2 py-1 bg-red-100 text-red-600 border border-red-300 rounded-full shadow-sm">
+bug
+</span>
+
+<span class="px-2 py-1 bg-yellow-100 text-yellow-700 border border-yellow-300 rounded-full shadow-sm">
+help wanted
+</span>
+
+</div>
+
+<hr class="mb-3">
+
+<!-- underline -->
+
+<hr class="mb-3">
+
+
+<!-- footer -->
+
+<div class="text-xs text-gray-500">
+
+<p># by ${issue.author}</p>
+<p>${new Date(issue.createdAt).toLocaleDateString()}</p>
+
+</div>
+
+</div>
+
 `
 
 })
 
 }
+
 
 function changeTab(tab){
 
